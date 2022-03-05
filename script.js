0838a575a4305d2707cfe7122be10c8c35597b5d
ConorDavison <conorjamesdavison@hotmail.co.uk>
2022-03-05 12:35:53 +0000

    added scroll to top button and made about p responsive

--- script.js
+++ script.js
@@ -3,5 +3,13 @@ $(document).ready(function(){
   $('#wrapper').fadeIn(1000);
 });
 
+// scrolltotop button
+//Get the button:
+mybutton = document.getElementById("myBtn");
+
+mybutton.addEventListener("click", function () {
+  $('html, body').animate({ scrollTop: 0 }, "slow");
+});
+
 
   
