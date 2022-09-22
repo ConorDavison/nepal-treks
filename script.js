6b00ed85afac2bab2742a3785bd09afab509d5d9
ConorDavison <conorjamesdavison@hotmail.co.uk>
2022-09-22 10:21:32 +0000

    added collapisble functionality to Divs using Js

--- script.js
+++ script.js
@@ -12,10 +12,21 @@ mybutton.addEventListener("click", function () {
 });
 
 document.body.onload = function() {
-  alert('Hello Trekkers!');
+  alert('Hello Trekkers! Welcome to Nepal Treks');
 }
 
 
+var coll = document.getElementsByClassName("collapsible");
+var i;
 
-
-
+for (i = 0; i < coll.length; i++) {
+  coll[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var content = this.nextElementSibling;
+    if (content.style.display === "block") {
+      content.style.display = "none";
+    } else {
+      content.style.display = "block";
+    }
+  });
+}
